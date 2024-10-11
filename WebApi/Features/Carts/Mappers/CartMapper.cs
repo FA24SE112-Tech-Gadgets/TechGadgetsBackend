4dc6e6c8c05ad0e3dffb6a2678be0ff6301d6da1
kietpt2003 <tuankiet29012003@gmail.com>
2024-10-11 17:34:20 +0700

    Finish cart

---
+++ WebApi/Features/Carts/Mappers/CartMapper.cs
@@ -0,0 +1,36 @@
+﻿using WebApi.Data.Entities;
+using WebApi.Features.Carts.Models;
+
+namespace WebApi.Features.Carts.Mappers;
+
+public static class CartMapper
+{
+    public static ICollection<CartGadgetItemResponse>? ToCartListResponse(this ICollection<CartGadget> cartList)
+    {
+        if (cartList != null)
+        {
+            return cartList
+            .Select(gadgetItem => new CartGadgetItemResponse
+            {
+                CartId = gadgetItem.CartId,
+                GadgetId = gadgetItem.GadgetId,
+                Quantity = gadgetItem.Quantity,
+            })
+            .ToList();
+        }
+        return null;
+    }
+
+    public static CartResponse? ToCartResponse(this Cart cart)
+    {
+        if (cart != null)
+        {
+            return new CartResponse
+            {
+                Id = cart.Id,
+                CartGadgets = cart.CartGadgets.ToCartListResponse()!,
+            };
+        }
+        return null;
+    }
+}
---
+++ WebApi/Features/Carts/Models/CartGadgetItemResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Features.Carts.Models;
+
+public class CartGadgetItemResponse
+{
+    public Guid CartId { get; set; }
+    public Guid GadgetId { get; set; }
+    public int Quantity { get; set; }
+}
