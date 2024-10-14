4547af7d280661b6a5b8c5ebe5e2551932a111ab
kietpt2003 <tuankiet29012003@gmail.com>
2024-10-14 07:16:04 +0700

    Create Favorite Gadget Validator

---
+++ WebApi/Features/FavoriteGadgets/CustomerCreateFavoriteGadget.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Common.Filters;
+using WebApi.Data.Entities;
+
+namespace WebApi.Features.FavoriteGadgets;
+
+[ApiController]
+[JwtValidation]
+[RolesFilter(Role.Customer)]
+[RequestValidation<Request>]
+public class CustomerCreateFavoriteGadget : ControllerBase
+{
+    public new class Request
+    {
+        public Guid CustomerId { get; set; }
+        public Guid GadgetId { get; set; }
+    }
+
+    public class Validator : AbstractValidator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(fg => fg.CustomerId)
+                .NotEmpty()
+                .WithMessage("CustomerId không được để trống");
+            RuleFor(fg => fg.GadgetId)
+                .NotEmpty()
+                .WithMessage("GadgetId không được để trống");
+        }
+    }
+}
