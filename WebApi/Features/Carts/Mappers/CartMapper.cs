cf05b23ae8efa7ea091c7c5643a680ec3efe2fa8
kietpt2003 <tuankiet29012003@gmail.com>
2024-10-14 19:42:25 +0700

    Add response models

--- WebApi/Features/Carts/Mappers/CartMapper.cs
+++ WebApi/Features/Carts/Mappers/CartMapper.cs
@@ -5,7 +5,7 @@ namespace WebApi.Features.Carts.Mappers;
 
 public static class CartMapper
 {
-    public static ICollection<CartGadgetItemResponse>? ToCartListResponse(this ICollection<CartGadget> cartList)
+    private static ICollection<CartGadgetItemResponse>? ToCartListResponse(this ICollection<CartGadget> cartList)
     {
         if (cartList != null)
         {
@@ -13,8 +13,8 @@ public static class CartMapper
             .Select(gadgetItem => new CartGadgetItemResponse
             {
                 CartId = gadgetItem.CartId,
-                GadgetId = gadgetItem.GadgetId,
                 Quantity = gadgetItem.Quantity,
+                Gadget = gadgetItem.Gadget.ToCartItemResponse()!,
             })
             .ToList();
         }
@@ -33,4 +33,85 @@ public static class CartMapper
         }
         return null;
     }
+
+    private static CartItemResponse? ToCartItemResponse(this Gadget gadget)
+    {
+        if (gadget != null)
+        {
+            return new CartItemResponse
+            {
+                Id = gadget.Id,
+                Name = gadget.Name,
+                Price = gadget.Price,
+                ThumbnailUrl = gadget.ThumbnailUrl,
+                Status = gadget.Status,
+                Condition = gadget.Condition,
+                Quantity = gadget.Quantity,
+                Seller = gadget.Seller.ToSellerCartResponse()!,
+                Brand = gadget.Brand.ToBrandcartResponse()!,
+                Category = gadget.Category.ToCategoryCartResponse()!,
+            };
+        }
+        return null;
+    }
+
+    private static SellerCartResponse? ToSellerCartResponse(this Seller seller)
+    {
+        if (seller != null)
+        {
+            return new SellerCartResponse
+            {
+                Id = seller.Id,
+                CompanyName = seller.CompanyName,
+                ShopName = seller.ShopName,
+                ShopAddress = seller.ShopAddress,
+                BusinessModel = seller.BusinessModel,
+                PhoneNumber = seller.PhoneNumber,
+                User = seller.User.ToUserCartResponse()!,
+            };
+        }
+        return null;
+    }
+
+    private static UserCartResponse? ToUserCartResponse(this User user)
+    {
+        if (user != null)
+        {
+            return new UserCartResponse
+            {
+                Id = user.Id,
+                Role = user.Role,
+                Status = user.Status,
+            };
+        }
+        return null;
+    }
+
+    private static BrandCartResponse? ToBrandcartResponse(this Brand brand)
+    {
+        if (brand != null)
+        {
+            return new BrandCartResponse
+            {
+                Id = brand.Id,
+                Name = brand.Name,
+                LogoUrl = brand.LogoUrl,
+            };
+        }
+        return null;
+    }
+
+    private static CategoryCartResponse? ToCategoryCartResponse(this Category category)
+    {
+        if (category != null)
+        {
+            return new CategoryCartResponse
+            {
+                Id = category.Id,
+                Name = category.Name,
+            };
+        }
+        return null;
+    }
+
 }
--- WebApi/Features/Carts/Models/CartGadgetItemResponse.cs
+++ WebApi/Features/Carts/Models/CartGadgetItemResponse.cs
@@ -3,6 +3,6 @@
 public class CartGadgetItemResponse
 {
     public Guid CartId { get; set; }
-    public Guid GadgetId { get; set; }
     public int Quantity { get; set; }
+    public CartItemResponse Gadget { get; set; } = default!;
 }
