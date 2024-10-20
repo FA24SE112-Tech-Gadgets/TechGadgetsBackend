6c120d6ef7fe691563bd854e75ed721fa3d97910
forevpurity <auvv310103@gmail.com>
2024-10-20 21:14:58 +0700

    Add GadgetThumbnailUrl to GadgetInformation

---
+++ WebApi/Data/Migrations/20241020141143_AddGadgetThumbnailUrlToGadgetInformation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGadgetThumbnailUrlToGadgetInformation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GadgetThumbnailUrl",
+                table: "GadgetInformation",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GadgetThumbnailUrl",
+                table: "GadgetInformation");
+        }
+    }
+}
