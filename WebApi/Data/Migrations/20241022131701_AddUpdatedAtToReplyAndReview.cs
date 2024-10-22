12438ac7159e4bd7eb87dc6b5151d99f7c1758c5
forevpurity <auvv310103@gmail.com>
2024-10-22 20:19:35 +0700

    Add UpdatedAt to Reply and Review

---
+++ WebApi/Data/Migrations/20241022131701_AddUpdatedAtToReplyAndReview.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtToReplyAndReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "SellerReplies",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Reviews",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "SellerReplies");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Reviews");
+        }
+    }
+}
