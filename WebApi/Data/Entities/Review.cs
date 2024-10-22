80621fc1c432b1989d9a0545caaf1d0256c0bac0
forevpurity <auvv310103@gmail.com>
2024-10-22 21:22:25 +0700

    Add OrderDetailId to Review

--- WebApi/Data/Entities/Review.cs
+++ WebApi/Data/Entities/Review.cs
@@ -5,6 +5,7 @@ public class Review
     public Guid Id { get; set; }
     public Guid CustomerId { get; set; }
     public Guid GadgetId { get; set; }
+    public Guid OrderDetailId { get; set; }
     public int Rating { get; set; }
     public string Content { get; set; } = default!;
     public bool IsPositive { get; set; }
@@ -12,6 +13,7 @@ public class Review
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
+    public OrderDetail OrderDetail { get; set; } = default!;
     public Customer Customer { get; set; } = default!;
     public Gadget Gadget { get; set; } = default!;
     public SellerReply? SellerReply { get; set; }
